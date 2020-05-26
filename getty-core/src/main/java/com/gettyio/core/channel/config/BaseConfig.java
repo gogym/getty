1732c6dee60efab772e7aba183fe7c67e11608bf
gogym <34082822+gogym@users.noreply.github.com>
2020-05-26 17:41:40 +0800

    Update BaseConfig.java

--- getty-core/src/main/java/com/gettyio/core/channel/config/BaseConfig.java
+++ getty-core/src/main/java/com/gettyio/core/channel/config/BaseConfig.java
@@ -47,7 +47,7 @@ public class BaseConfig {
     /**
      * 版本
      */
-    public static final String VERSION = "1.3.4";
+    public static final String VERSION = "1.3.5";
     /**
      * 服务器地址
      */
